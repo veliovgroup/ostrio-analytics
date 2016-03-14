3f23c07ba66e22a0d1617b5aeed19b46f124e591
Dmitriy A. Golev <ceo@veliov.com>
2016-03-14 05:46:32 +0300

    v1.0.0

---
+++ package.js
@@ -0,0 +1,13 @@
+Package.describe({
+  name: 'ostrio:analytics',
+  version: '1.0.0',
+  summary: 'Visitor\'s analytics tracking code for ostr.io service',
+  git: 'https://github.com/VeliovGroup/ostrio-analytics',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.1');
+  api.addFiles('./lib/analytics.js', 'client');
+  api.export('OstrioTrackerClass');
+});
