7f77e283e61ec45850479d797212f26f588faf48
dr-dimitru <ceo@veliov.com>
2016-11-01 03:32:57 +0300

    v1.1.0
     - Support for latest ostr.io service update
     - More lightweight solution
     - Thin code, less than 2.5KB

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'ostrio:analytics',
-  version: '1.0.3',
+  version: '1.1.0',
   summary: 'Visitor\'s analytics tracking code for ostr.io service',
   git: 'https://github.com/VeliovGroup/ostrio-analytics',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0');
-  api.addFiles('./lib/analytics.js', 'client');
+  api.versionsFrom('1.4');
+  api.mainModule('./lib/analytics.js', 'client');
   api.export('OstrioTrackerClass', 'client');
 });
