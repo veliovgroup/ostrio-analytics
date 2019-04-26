a0cea5747827744916b67f7f29e728ac46181552
dr.dimitru <ceo@veliov.com>
2019-04-26 02:47:58 -0700

    Atmosphere: v1.2.8
     - 📦 Internal Meteor dependencies update
     - 🤝 Compatibility with `meteor@1.8.1`
     - 👷‍♂️ Maintenance release

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
   name: 'ostrio:analytics',
-  version: '1.2.7',
+  version: '1.2.8',
   summary: 'Visitor\'s web analytics tracking code for ostr.io service',
   git: 'https://github.com/VeliovGroup/ostrio-analytics',
   documentation: 'README.md'
 });
 
-Package.onUse(function(api) {
+Package.onUse((api) => {
   api.versionsFrom('1.4');
   api.use('ecmascript', 'client');
   api.mainModule('./lib/meteor.js', 'client');
 });
 
 Npm.depends({
-  'ostrio-analytics': '1.1.7'
+  'ostrio-analytics': '1.1.8'
 });
